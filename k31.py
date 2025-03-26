c915864d9758efb2f2ddb0b634ed89218f76c90d
MoulikArora21 <moulikarora12@gmail.com>
2025-03-26 15:58:47 +0200

    Turn fixed, depth added, started minmax

--- k31.py
+++ k31.py
@@ -10,16 +10,13 @@ for x in range(lenstr):
 #rand_str = ''.join(str(x) for x in rand_list)
 
 intturn = int(input("Choose 1 for P1, 0 for P2: "))
-if intturn == 0:
-    turn = False
-else:
-    turn = True
 
 class Node:
     def __init__(self, child_value):
         self.value = child_value
         self.children = []
         self.heuristic = 0
+
         
     def add_node(self,child):
         self.children.append(child)
@@ -37,14 +34,30 @@ class Node:
             
         
 
-root = Node({"P1Score": 100, "StateString": rand_list, "P2Score": 100})
+root = Node({"P1Score": 100, "StateString": rand_list, "P2Score": 100, "Depth": 1})
 print(root.value)
 
 
-def calculate_score(curr_node,chosen_value,turn):
+def calculate_score(curr_node,chosen_value):
     x = curr_node.value.copy()
     y = curr_node.value["StateString"].copy()
-    child = Node({"P1Score": x["P1Score"], "StateString":y, "P2Score": x["P2Score"]})
+
+    child = Node({"P1Score": x["P1Score"], "StateString":y, "P2Score": x["P2Score"], "Depth":x["Depth"]})
+    child.value["Depth"]+=1
+
+
+    if intturn == 1:  
+        if (len(curr_node.value["StateString"])%2 == len(root.value["StateString"])%2):
+            turn = True
+        else:
+            turn = False
+    else:
+        if (len(curr_node.value["StateString"])%2 == len(root.value["StateString"])%2):
+            turn = False
+        else:
+            turn = True
+
+
 
     if turn == True:
         if chosen_value%2 == 0:
@@ -62,13 +75,16 @@ def calculate_score(curr_node,chosen_value,turn):
     child.value["StateString"] = y
 
     curr_node.add_node(child)
-    child.add_heuristic()
     return child
 
 
+depth = 1
 
 def generate_list(curr_node):
     global turn
+    global depth
+
+    
     curr_list = curr_node.value["StateString"]
 
     if(len(curr_list) == 0):
@@ -78,18 +94,33 @@ def generate_list(curr_node):
     
     avail_options = set(curr_node.value["StateString"])
     for i in (avail_options):
-
-        if len(root.value["StateString"]) == len(curr_node.value["StateString"]):
-            turn = turn
-        else:
-            turn = not turn
-
-        child = calculate_score(curr_node,i,turn)
-        print(child.value)
-        print(child.heuristic)
-        generate_list(child)
             
 
+        child = calculate_score(curr_node,i)
+        print(child.value)
+        if (child.value["Depth"]> 3):
+            return
+
+        generate_list(child)
     return
 
 generate_list(root) 
+
+
+def minmax(root):
+    for w in root.children:
+        for x in w.children:
+            for y in x.children:
+                y.add_heuristic()
+                print(y.value , y.heuristic)
+
+    for w in root.children:
+        for x in w.children:
+            for y in x.children:
+                maxh = 0
+                if y.heuristic>maxh:
+                    maxh = y.heuristic
+            x.heuristic = maxh
+            print(x.value, x.heuristic)
+
+minmax(root)
