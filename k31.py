4a8a1e4cd683d978cfa10c36c79532a43f8e0c56
MoulikArora21 <moulikarora12@gmail.com>
2025-03-29 15:44:07 +0200

    lots of progress

--- k31.py
+++ k31.py
@@ -9,7 +9,7 @@ for x in range(lenstr):
 
 #rand_str = ''.join(str(x) for x in rand_list)
 
-intturn = int(input("Choose 1 for P1, 0 for P2: "))
+intturn = int(input("Choose 1 for Your Turn, 0 for Computer's Turn: "))
 
 class Node:
     def __init__(self, child_value):
@@ -24,13 +24,34 @@ class Node:
 
     def add_heuristic(self):
         hv = 0
-        for values in self.value["StateString"] :
-            if values%2 == 0:
-                hv +=1
+        if (self.value["StateString"] != []):
+            for values in self.value["StateString"] :
+                if values%2 == 0:
+                    hv +=1
 
-            if values%3 == 0 or values%4 == 0:
-                hv +=1
-        self.heuristic=hv
+                if values%3 == 0 or values%4 == 0:
+                    hv +=1
+            if intturn == 1:
+                dif = self.value["P2Score"] - self.value["P1Score"] 
+                hv+=dif
+            else:
+                dif = self.value["P2Score"] - self.value["P1Score"] 
+                hv+=dif
+            self.heuristic=hv
+        else:
+            if((self.value["P1Score"] - self.value["P2Score"]) < 0):
+                if intturn == 1:
+                    self.heuristic = -1
+                else:
+                    self.heuristic = 1
+            elif(self.value["P1Score"] - self.value["P2Score"] == 0):
+                self.heuristic = 0
+            else:
+                if intturn ==1:
+                    self.heuristic = 1
+                else:
+                    self.heuristic = -1
+        
             
         
 
@@ -56,6 +77,9 @@ def calculate_score(curr_node,chosen_value):
             turn = False
         else:
             turn = True
+    # print(f"From: {curr_node.value}")
+    # print(f"Chosen Value: {chosen_value}, Turn: {'P1' if turn else 'P2'}")
+    # print(f"Current Node: {curr_node.value}, Generated Child Node: {child.value}")
 
 
 
@@ -78,11 +102,11 @@ def calculate_score(curr_node,chosen_value):
     return child
 
 
-depth = 1
+max_depth =4
 
 def generate_list(curr_node):
     global turn
-    global depth
+    global max_depth
 
     
     curr_list = curr_node.value["StateString"]
@@ -97,30 +121,147 @@ def generate_list(curr_node):
             
 
         child = calculate_score(curr_node,i)
-        print(child.value)
-        if (child.value["Depth"]> 3):
+        # print(child.value)
+        if (child.value["Depth"] > max_depth ):
             return
 
         generate_list(child)
+        # print(f"Current Node: {curr_node.value}, Generated Child Node: {child.value}")
+
     return
 
 generate_list(root) 
 
 
-def minmax(root):
+def add_heuristic_to_leaves(root):
     for w in root.children:
         for x in w.children:
             for y in x.children:
                 y.add_heuristic()
-                print(y.value , y.heuristic)
+                # print(y.value , y.heuristic)
 
-    for w in root.children:
-        for x in w.children:
-            for y in x.children:
-                maxh = 0
-                if y.heuristic>maxh:
-                    maxh = y.heuristic
-            x.heuristic = maxh
-            print(x.value, x.heuristic)
 
-minmax(root)
+def propagate(node,root):
+    if (len(node.value["StateString"])%2 == len(root.value["StateString"])%2):
+        for w in node.children:
+            for x in w.children:
+                maxh = -float('inf')
+                for y in x.children:
+                    if y.heuristic>maxh:
+                        maxh = y.heuristic
+                x.heuristic = maxh
+                #print(x.value , x.heuristic)
+            maxh2 = float('inf')
+            for x in w.children:
+                if x.heuristic<maxh2:
+                    maxh2 = x.heuristic
+            w.heuristic = maxh2
+            #print(w.value , w.heuristic)
+        maxh3 = -float('inf')
+        for w in node.children:
+            if w.heuristic>maxh3:
+                maxh3 = w.heuristic
+        node.heuristic = maxh3
+        # print(root.value , root.heuristic)
+    else:
+        for w in node.children:
+            for x in w.children:
+                maxh = float('inf')
+                for y in x.children:
+                    if y.heuristic<maxh:
+                        maxh = y.heuristic
+                x.heuristic = maxh
+                #print(x.value , x.heuristic)
+            maxh2 = -float('inf')
+            for x in w.children:
+                if x.heuristic>maxh2:
+                    maxh2 = x.heuristic
+            w.heuristic = maxh2
+            #print(w.value , w.heuristic)
+        maxh3 = float('inf')
+        for w in node.children:
+            if w.heuristic<maxh3:
+                maxh3 = w.heuristic
+        node.heuristic = maxh3
+                        
+
+def minmax(node,root):
+    add_heuristic_to_leaves(node) # step 2
+    propagate(node,root)  
+
+minmax(root,root)
+
+
+def computer_move(node):
+    # if (node.value["StateString"] == []):
+    #     return None
+
+    # if node.children == None:
+    #     return None
+
+    bestmove = None
+    bestheur = -float("inf")
+    for x in node.children:
+        if x.heuristic>bestheur:
+            bestheur = x.heuristic
+            bestmove = x
+
+    for options in node.children:
+         if options.heuristic==bestheur:
+            print(options.value, options.heuristic)
+    return bestmove
+    
+        
+def player_move(node):
+    # if (node.value["StateString"] == []):
+    #     return None
+
+    # if node.children == None:
+    #     return None
+    print("Choose a number")
+    choice = int(input())
+    temp_list = node.value["StateString"].copy()
+    temp_list.remove(choice)
+    chosen_node = None
+    for x in node.children:
+        if (temp_list == x.value["StateString"]):
+            print(x.value)
+            return x
+
+def generate_further(node,root):
+    global max_depth
+    max_depth +=4
+    generate_list(node)
+    print(node.value, node.children[0])
+    minmax(node,root)
+    return node
+
+actual_root = Node({"P1Score": 100, "StateString": rand_list, "P2Score": 100, "Depth": 1})
+if intturn == 1:
+    while root.value["StateString"] != [] or root!= None:
+        x = player_move(root)
+        if (x == None or x.value["StateString"] == []):
+            break
+        y = computer_move(x)
+        if (y == None or y.value["StateString"] == []):
+            break
+        z = player_move(y)
+        a = generate_further(z,actual_root)
+        w = computer_move(a)
+        if (w == None or w.value["StateString"] == []):
+            break
+        root = w
+
+else:
+    while root.value["StateString"] != [] and root!= None:
+        x = computer_move(root)
+        # if (x == None or x.value["StateString"] == []):
+        #     break
+        y = player_move(x)
+        # if (y == None or y.value["StateString"] == []):
+        #     break
+        z = computer_move(y)
+        a = generate_further(z,actual_root)
+        w = player_move(a)
+        root = w
+
